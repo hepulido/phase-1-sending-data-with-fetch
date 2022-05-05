0fc4e45977089e91e350b717f7a4818a14b5e7c1
hector <hpulido1021@gmail.com>
2022-05-05 15:45:50 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email){
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  };
+
+return fetch('http://localhost:3000/users', configurationObject)
+.then((resp) => resp.json())
+.then(user => document.body.innerHTML = user.id)
+.catch (error => {
+    alert("Wrong name! Please try again!");
+    console.log(error.message);
+    document.body.append(error.message)
+  });
+}  
+
+
+
